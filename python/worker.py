e945dadfd5f5d350d65962786a214c03acf94109
Walter Moreira <walter@waltermoreira.net>
2014-05-29 17:09:07 -0500

    Improve structure of python worker

--- python/worker.py
+++ python/worker.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python
+import argparse
 import sys
 import time
 
 import pika
 
 
-def fun():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
-    channel = connection.channel()
+class Worker(object):
 
-    channel.queue_declare(queue='my', durable=True)
-    print ' [*] Waiting for messages. To exit press CTRL+C'
+    QUEUE_NAME = 'tasks'
 
-    def callback(ch, method, properties, body):
-        print " [x] Received %r" % (body,)
-        time.sleep( body.count('.') )
-        print " [x] Done"
-        print " answering to routing_key", properties.reply_to
-        ch.basic_publish(exchange='',
-                         routing_key=properties.reply_to,
-                         body='hello ' + body)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+    def __init__(self, queue_host, queue_name=None):
+        self.queue_host = queue_host
+        self.queue_name = queue_name or self.QUEUE_NAME
+        self.connect()
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(callback, queue='my')
+    def connect(self):
+        """Establish a connection with the task queue."""
 
-    channel.start_consuming()
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.queue_host))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
+
+    def run(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(self.callback, queue=self.queue_name)
+        self.channel.start_consuming()
+
+    @staticmethod
+    def callback(channel, method, properties, body):
+        print('Received:', body)
+        channel.basic_publish(exchange='',
+                              routing_key=properties.reply_to,
+                              body=body)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='A Python worker')
+    parser.add_argument('--queue-host', required=True, metavar='HOST',
+                        help='host where RabbitMQ is running')
+    return parser.parse_args()
+
+def main():
+    hostname = parse_args().queue_host
+    worker = Worker(hostname)
+    print('Worker starting')
+    worker.run()
 
 if __name__ == '__main__':
-    print(sys.argv)
+    main()
