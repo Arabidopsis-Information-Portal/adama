55033ac1498f9abd30cae3c20da5ea2a9b01a8ee
Walter Moreira <walter@waltermoreira.net>
2014-05-29 15:49:20 -0500

    Launch python container as a worker

---
+++ python/worker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import time
+
+import pika
+
+
+def fun():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='my', durable=True)
+    print ' [*] Waiting for messages. To exit press CTRL+C'
+
+    def callback(ch, method, properties, body):
+        print " [x] Received %r" % (body,)
+        time.sleep( body.count('.') )
+        print " [x] Done"
+        print " answering to routing_key", properties.reply_to
+        ch.basic_publish(exchange='',
+                         routing_key=properties.reply_to,
+                         body='hello ' + body)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback, queue='my')
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    print(sys.argv)
