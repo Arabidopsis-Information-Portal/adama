94261ff939144658ade86d6c390083b8f62fb514
Walter Moreira <walter@waltermoreira.net>
2014-06-06 17:09:53 -0500

    Remove unused import

--- apim/__init__.py
+++ apim/__init__.py
@@ -5,7 +5,7 @@ __author__ = 'Walter Moreira'
 __email__ = 'wmoreira@tacc.utexas.edu'
 __version__ = '0.1.0'
 
-from flask import Flask, request
+from flask import Flask
 from flask.ext import restful
 
 from .api import Query, Register
