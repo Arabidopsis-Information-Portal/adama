a381a69564be13d0309e9f519cb8518918ed668d
Walter Moreira <walter@waltermoreira.net>
2014-06-11 17:04:35 -0500

    Start adding swagger support

--- apim/__init__.py
+++ apim/__init__.py
@@ -7,11 +7,17 @@ __version__ = '0.1.0'
 
 from flask import Flask
 from flask.ext import restful
+from flask_restful_swagger import swagger
 
 from .api import Query, Register
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = swagger.docs(restful.Api(app),
+                   apiVersion='0.1',
+                   basePath='http://localhost:5000',
+                   resourcePath='/',
+                   produces=["application/json", "text/html"],
+                   api_spec_url='/api/spec')
 
 api.add_resource(Query, '/query')
 api.add_resource(Register, '/register')
