b8a37f138ae41dc5a310c8ef1063cf76e03253e8
Walter Moreira <walter@waltermoreira.net>
2014-06-11 11:32:12 -0500

    Real example of a data source
    Implement "Expresslogs_by_locus" as detailed in https://github.com/Arabidopsis-Information-Portal/araport_data_api_design/blob/master/Expressologs_by_locus.txt

--- apim/adapter/adapters/thalemine/main.py
+++ apim/adapter/adapters/thalemine/main.py
@@ -1,13 +1,62 @@
 import json
+import urlparse
 
 import requests
 
+URL = 'http://bar.utoronto.ca/webservices/get_expressologs.php'
+
 
 def process(args):
+    """Standard JSON object `args` has the form::
 
-    print(json.dumps({'hey': 'there',
-                      'key': args}, indent=4))
-    print '---'
-    print(json.dumps(kwargs))
-    print '---'
+        {"query": <query>,
+         "countOnly": False,
+         "pageSize": 100,
+         "page": 1}
+
+    where `<query>` is AIP formatted query.
+
+    """
+
+    query = args['query']
+    locus = query['locus']
+    param = '[{{"gene":"{locus}"}}]'.format(locus=locus)
+
+    # original response from 3rd party service
+    native_response = requests.get(URL, params={'request': param}).json()
+
+    hits = native_response[locus]
+    for hit in hits:
+        process_one(hit, locus)
+
     print('END')
+
+
+def process_one(hit, locus):
+    """Process one entry of the response."""
+
+    # parse `efp_link` url to extract query parameters
+    efp_link = hit['efp_link']
+    efp_query = urlparse.urlparse(efp_link).query
+    parsed_efp_query = urlparse.parse_qs(efp_query)
+
+    coexpression_result = {
+        'class': 'locus_relationship',
+        'reference': 'TAIR10',
+        'locus': locus,
+        'type': 'coexpression',
+        'related_entity': parsed_efp_query['primaryGene'],
+        'direction': 'undirected',
+        'score': [
+            {'correlation_coefficient': hit['correlation_coefficient']}],
+        'source': parsed_efp_query['dataSource']}
+    print(json.dumps(coexpression_result, indent=4))
+    print('---')
+
+    similarity_result = coexpression_result
+    similarity_result.update(
+        {'type': 'similarity',
+         'score': [
+             {'similarity_percentage': hit['seq_similarity']}]})
+    print(json.dumps(similarity_result, indent=4))
+    print('---')
