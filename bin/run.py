42bcd6491914bc2e9a707ac361d248085008e80b
Walter Moreira <walter@waltermoreira.net>
2014-06-23 17:28:55 -0500

    Add script to run gunicorn server (WIP)

--- bin/run.py
+++ bin/run.py
@@ -1,4 +1,44 @@
-from apim import app
+import subprocess
+
+import pika
+
+from apim.config import Config
+
+
+def check_queue():
+    """Check that the queue exchange is running.
+
+    Use the info from the config file.
+
+    Do more than just check the connection, try to register a
+    queue and then delete it.
+
+    """
+    try:
+        conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host=Config.get('queue', 'host'),
+                                      port=Config.getint('queue', 'port')))
+        channel = conn.channel()
+        channel.queue_declare(queue='test')
+        channel.queue_delete(queue='test')
+        return True
+    except pika.AMQPError:
+        return False
+
+
+def check_docker():
+    """Check that the Docker daemon is running.
+
+    Use the info from the config file.
+
+    """
+    pass
+
+
+def run():
+    """Run the http server."""
+    pass
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    run()
