c35090426e13e2717e1d456960e95153b1ecb499
Walter Moreira <walter@waltermoreira.net>
2014-06-24 16:59:05 -0500

    Add a main 'run' command

--- bin/run.py
+++ bin/run.py
@@ -1,15 +1,31 @@
-import subprocess
+#!/usr/bin/env python
 
-import pika
+import subprocess
+import sys
 
 from apim.config import Config
 from apim.docker import check_docker
 from apim.tasks import check_queue
 
 
+def run_gunicorn():
+    subprocess.check_call(
+        ['gunicorn', '-w',
+         Config.get('server', 'workers'),
+         '-t',
+         Config.get('server', 'timeout'),
+         '-k', 'gevent', 'apim:app'])
+
+
 def run():
     """Run the http server."""
-    pass
+
+    docker_alive = check_docker(display=True)
+    queue_alive = check_queue(display=True)
+    if not (docker_alive and queue_alive):
+        sys.exit(1)
+
+    run_gunicorn()
 
 
 if __name__ == '__main__':
