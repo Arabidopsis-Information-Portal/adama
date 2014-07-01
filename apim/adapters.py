5b63411d9906ca314a34e1da4d44fa226c28eeda
Walter Moreira <walter@waltermoreira.net>
2014-06-30 20:01:58 -0500

    Implement list_all endpoint for adapters

--- apim/adapters.py
+++ apim/adapters.py
@@ -3,6 +3,7 @@ import cPickle
 import redis
 
 from .config import Config
+from .adapter import Adapter
 
 
 class Adapters(dict):
@@ -18,7 +19,17 @@ class Adapters(dict):
         self._db.set(adapter.iden, obj)
 
     def list_all(self):
-        return self._db.keys()
+        return list(self.get_adapters())
 
+    def get_adapters(self):
+        for key in self._db.keys():
+            obj = cPickle.loads(self._db.get(key))
+            yield {
+                'identifier': obj.iden,
+                'version': obj.version,
+                'metadata': obj.metadata,
+                'language': obj.language,
+                'workers': obj.workers
+            }
 
 adapters = Adapters()
