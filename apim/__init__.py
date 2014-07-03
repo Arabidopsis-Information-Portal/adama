1037efb371c65b5a00324f6ffcfb9cc3e55f5278
Walter Moreira <walter@waltermoreira.net>
2014-07-03 12:37:30 -0500

    Change hardcoded value to config option

--- apim/__init__.py
+++ apim/__init__.py
@@ -15,11 +15,12 @@ app = Flask(__name__)
 from .query import Query
 from .register import Register
 from .api import MyApi
+from .config import Config
 
 
 api = swagger.docs(MyApi(app),
                    apiVersion='0.1',
-                   basePath='http://localhost:8000',
+                   basePath=Config.get('server', 'url'),
                    resourcePath='/',
                    produces=["application/json", "text/html"],
                    api_spec_url='/api/spec')
