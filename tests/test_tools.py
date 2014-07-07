d60c1c3809b057cb5e7f8fdde91a2bac64479879
Walter Moreira <walter@waltermoreira.net>
2014-07-07 10:53:52 -0500

    Add tests for timer

---
+++ tests/test_tools.py
@@ -0,0 +1,27 @@
+import time
+
+import pytest
+
+import apim.tools as t
+
+@pytest.fixture(scope='module')
+def f():
+    def _f(t):
+        def __f():
+            time.sleep(t)
+            return True
+        return __f
+    return _f
+
+def test_timeout_0(f):
+    g = t.TimeoutFunction(f(1), 0)
+    assert g()
+
+def test_function_wins(f):
+    g = t.TimeoutFunction(f(0.1), 0.2)
+    assert g()
+
+def test_timeout_wins(f):
+    g = t.TimeoutFunction(f(0.2), 0.1)
+    with pytest.raises(t.TimeoutFunctionException):
+        g()
