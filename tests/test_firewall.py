12e421d556fd7fc401138a3f25da377f313299ab
Walter Moreira <walter@waltermoreira.net>
2014-08-01 16:33:30 -0500

    Add tests for firewall refresh

--- tests/test_firewall.py
+++ tests/test_firewall.py
@@ -81,3 +81,41 @@ def test_missing():
                    insert=check_insert,
                    delete=lambda _: None)
     w._ensure_drop('foo')
+
+def test_refresh():
+    table = dedent(
+        """
+        Chain FORWARD (policy ACCEPT 0 packets, 0 bytes)
+        num   pkts bytes target     prot opt in     out     source               destination
+        1        0     0 ACCEPT     all  --  *      *       0.0.0.0/0            1.1.1.1              PHYSDEV match --physdev-in foo
+        2        0     0 DROP       all  --  *      *       0.0.0.0/0            0.0.0.0/0            PHYSDEV match --physdev-in foo
+        """)
+
+    def check_insert(line, dest, iface, target):
+        assert line == 1
+        assert dest == '2.2.2.2'
+        assert iface == 'foo'
+        assert target == 'ACCEPT'
+
+    w = f.Firewall(['1.1.1.1', '2.2.2.2'],
+                   get=lambda: table,
+                   insert=check_insert,
+                   delete=lambda _: None)
+    w._refresh('foo')
+
+    def check_insert(line, dest, iface, target):
+        assert line == 1
+        assert dest == '3.3.3.3'
+        assert iface == 'foo'
+        assert target == 'ACCEPT'
+
+    def check_delete(dest, iface, target):
+        assert dest == '1.1.1.1'
+        assert iface == 'foo'
+        assert target == 'ACCEPT'
+
+    w = f.Firewall(['3.3.3.3'],
+                   get=lambda: table,
+                   insert=check_insert,
+                   delete=check_delete)
+    w._refresh('foo')
