119c22896616b57445207ad7a75291006d3cc362
Walter Moreira <walter@waltermoreira.net>
2014-08-22 21:05:35 +0000

    Change register endpoint to async model

--- apim/register.py
+++ apim/register.py
@@ -1,12 +1,15 @@
+import json
+import multiprocessing
+
 from flask import request
 from flask.ext import restful
 from flask.ext.restful import reqparse
 from flask_restful_swagger import swagger
+import requests
 from werkzeug.exceptions import ClientDisconnected
 from werkzeug.datastructures import FileStorage
 
 from .adapter import Adapter
-from .adapters import adapters
 from .config import Config
 from .api import APIException
 from . import app
@@ -94,6 +97,14 @@ class Register(restful.Resource):
                 'paramType': 'form'
             },
             {
+                'name': 'notify',
+                'description': 'URL to notify when adapter is ready',
+                'required': False,
+                'allowMultiple': False,
+                'dataType': 'string',
+                'paramType': 'form'
+            },
+            {
                 'name': 'code',
                 'description': "user's code for the adapter",
                 'required': True,
@@ -106,25 +117,62 @@ class Register(restful.Resource):
     def post(self):
         app.logger.debug('/register received POST')
         args = self.validate()
-        metadata = {'name': args.name,
-                    'version': args.version or '0.1',
-                    'requirements': args.requirements or '',
-                    'url': args.url}
-        app.logger.debug('Starting adapter registration')
-        adapter = Adapter(args.code.filename, args.code.read(), metadata)
-        app.logger.debug(' created object')
-        adapter.register()
-        app.logger.debug(' registered')
-        adapter.start_workers()
-        app.logger.debug(' workers started')
-        adapter.check_health()
-        app.logger.debug(' all healthy')
-        adapters.add(adapter)
-        app.logger.debug(' recorded')
+        self.async_register(args)
         return {
             'status': 'success',
-            'result': adapter.to_json()
+            'message': ("registration started; will POST to '{}' when ready"
+                        .format(args.notify))
         }
+
+    def async_register(self, args):
+        try:
+            print("to spawn")
+            code = args.code.read()
+            proc = multiprocessing.Process(
+                target=self._register_adapter, args=(args, code))
+            proc.start()
+            print("after spawn")
+        except Exception as exc:
+            raise APIException(
+                "Failed to start registration process.\n"
+                "Original exception follows:\n"
+                "{}".format(exc),
+                500)
+
+    def _register_adapter(self, args, code):
+        try:
+            from .adapters import adapters
+            metadata = {'name': args.name,
+                        'version': args.version or '0.1',
+                        'requirements': args.requirements or '',
+                        'url': args.url,
+                        'notify': args.notify}
+            app.logger.debug('Starting adapter registration')
+            adapter = Adapter(args.code.filename, code, metadata)
+            app.logger.debug(' created object')
+            adapter.register()
+            app.logger.debug(' registered')
+            adapter.start_workers()
+            app.logger.debug(' workers started')
+            adapter.check_health()
+            app.logger.debug(' all healthy')
+            adapters.add(adapter)
+            app.logger.debug(' recorded')
+            data = {
+                'status': 'success',
+                'result': adapter.to_json()
+            }
+            print("success")
+        except Exception as exc:
+            data = {
+                'status': 'error',
+                'result': str(exc)
+            }
+            print ("exception")
+            print("-->", exc)
+        requests.post(args.notify,
+                      headers={"Content-Type": "application/json"},
+                      data=json.dumps(data))
 
     @swagger.operation(
         notes='List all registered adapters',
@@ -133,6 +181,7 @@ class Register(restful.Resource):
         parameters=[]
     )
     def get(self):
+        from .adapters import adapters
         app.logger.debug('/register received GET')
         return {'status': 'success',
                 'adapters': adapters.list_all()}
@@ -146,6 +195,7 @@ class Register(restful.Resource):
         parser.add_argument('url', type=str, required=True,
                             help='url of the data service is required')
         parser.add_argument('requirements', type=str)
+        parser.add_argument('notify', type=str)
         parser.add_argument('code', type=FileStorage, required=True,
                             location='files',
                             help='a file, tarball, or zip, must be uploaded')
