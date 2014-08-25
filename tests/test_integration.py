ddec2ad672042e2027e029adf53910286ee41dde
Walter Moreira <walter@waltermoreira.net>
2014-08-25 21:58:17 +0000

    Add test to check language is properly filled

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -33,6 +33,16 @@ def test_state():
             return
         time.sleep(5)
 
+def test_language():
+    response = requests.get('http://localhost/register').json()
+    assert response['status'] == 'success'
+    adapters = response['adapters']
+    for adapter in adapters:
+        if adapter['identifier'] == 'foo_v0.1':
+            assert adapter['language'] == 'python'
+            return
+    assert False
+
 def test_query():
     resp = requests.post('http://localhost/query',
                  data=json.dumps({
