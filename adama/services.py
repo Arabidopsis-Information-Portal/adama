5a7fce72cf5dc5c99ed666b0628af3a1b149a631
Walter Moreira <walter@waltermoreira.net>
2014-09-16 18:40:54 +0000

    Add parameter 'type' to services

--- adama/services.py
+++ adama/services.py
@@ -62,6 +62,7 @@ class ServicesResource(restful.Resource):
         parser.add_argument('requirements', type=str, action='append',
                             default=[])
         parser.add_argument('notify', type=str, default='')
+        parser.add_argument('type', type=str, default='QueryWorker')
         parser.add_argument('code', type=FileStorage, required=True,
                             location='files',
                             help='a file, tarball, or zip, must be uploaded')
