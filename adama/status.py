fa51e16c14c69dedfa5251701ecc03009fe6b640
Walter Moreira <walter@waltermoreira.net>
2014-09-24 20:56:36 +0000

    Report the hash of HEAD in /status
    This helps to check which version of Adama is currently serving the request.

--- adama/status.py
+++ adama/status.py
@@ -1,12 +1,25 @@
+import subprocess
+
 from flask.ext import restful
 
 from . import __version__
+from .api import ok
+from .tools import location_of
 
 
 class StatusResource(restful.Resource):
 
     def get(self):
-        return {
+        return ok({
             'api': 'Adama v{}'.format(__version__),
-            'status': 'success'
-        }
+            'hash': head_hash()
+        })
+
+
+def head_hash():
+    try:
+        return subprocess.check_output(
+            'cd {} && git rev-parse HEAD'.format(location_of(__file__)),
+            shell=True).strip()
+    except OSError:
+        return '<could not retrieve hash of HEAD commit>'
