24e5cf7a2205bc575ecf561fbf08ae2897581b38
Walter Moreira <walter@waltermoreira.net>
2014-10-01 11:48:58 -0500

    Improve reporting of errors
    Include full traceback of exception.

--- adama/api.py
+++ adama/api.py
@@ -33,38 +33,41 @@ class RegisterException(Exception):
 
 class MyApi(restful.Api):
 
-    def handle_error(self, e):
-        try:
-            raise e
-        except APIException as exc:
-            return self.make_response(
-                error({'message': 'API error: {0}'.format(exc.message)}),
-                exc.code)
-        except subprocess.CalledProcessError as exc:
-            return self.make_response(
-                error({'command': ' '.join(exc.cmd),
-                       'message': exc.output}),
-                500)
-        except RegisterException as exc:
+    def handle_error(self, exc):
+        if isinstance(exc, APIException):
+            return self.with_traceback(
+                {'message': 'API error: {0}'.format(exc.message)},
+                exc, code=exc.code)
+
+        if isinstance(exc, subprocess.CalledProcessError):
+            return self.with_traceback(
+                {'command': ' '.join(exc.cmd),
+                 'message': exc.output}, exc)
+
+        if isinstance(exc, RegisterException):
             all_logs = '\n---\n'.join(exc.logs)
-            return self.make_response(
-                error({'message': textwrap.dedent(
+            return self.with_traceback(
+                {'message': textwrap.dedent(
                     """
                     Workers failed to start: {0} out of {1}.
                     Logs follow:
 
                     {2}""").format(exc.failed_count,
                                    exc.total_workers,
-                                   all_logs)}),
-                500)
-        except Exception as exc:
+                                   all_logs)}, exc)
+
+        if isinstance(exc, Exception):
             child_tb = getattr(exc, 'child_traceback', None)
             trace = traceback.format_exc()
-            return self.make_response(
-                error({'trace': trace,
-                       'worker_trace': child_tb,
-                       'message': exc.message}),
-                500)
+            return self.with_traceback(
+                {'trace': trace,
+                 'worker_trace': child_tb,
+                 'message': exc.message}, exc)
+
+    def with_traceback(self, data, exc, code=500):
+        data['traceback'] = traceback.format_exc()
+        data['exception'] = exc.__class__.__name__
+        return self.make_response(error(data), code)
 
 
 def ok(obj):
