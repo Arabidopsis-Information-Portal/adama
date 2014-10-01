a6d52f3379944d1eb93dea8302b317a893bfe167
Walter Moreira <walter@waltermoreira.net>
2014-09-30 22:04:46 -0500

    Test extracting function

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,6 +1,7 @@
 import os
 import multiprocessing
 import tempfile
+import shutil
 
 import pytest
 import adama.services
@@ -15,8 +16,31 @@ HERE = location_of(__file__)
 def test_extract_module():
     mod = open(os.path.join(HERE, 'main.py')).read()
     temp = tempfile.mkdtemp()
-    user_code = adama.services.extract('mymain.py', mod, temp)
-    assert open(os.path.join(user_code, 'mymain.py')).read() == mod
+    try:
+        user_code = adama.services.extract('mymain.py', mod, temp)
+        assert open(os.path.join(user_code, 'mymain.py')).read() == mod
+    finally:
+        shutil.rmtree(temp, ignore_errors=True)
+
+def test_extract_tarball():
+    tarball = open(os.path.join(HERE, 'foo.tgz')).read()
+    temp = tempfile.mkdtemp()
+    try:
+        user_code = adama.services.extract('foo.tgz', tarball, temp)
+        assert open(
+            os.path.join(user_code, 'main.py')).read().startswith('foo')
+    finally:
+        shutil.rmtree(temp, ignore_errors=True)
+
+def test_extract_zip():
+    zipfile = open(os.path.join(HERE, 'foo.zip')).read()
+    temp = tempfile.mkdtemp()
+    try:
+        user_code = adama.services.extract('foo.zip', zipfile, temp)
+        assert open(
+            os.path.join(user_code, 'main.rb')).read().startswith('foo')
+    finally:
+        shutil.rmtree(temp, ignore_errors=True)
 
 def test_valid_image_name():
     assert adama.services.valid_image_name('foo')
@@ -53,29 +77,6 @@ def test_register():
     finally:
         service_store['foo_ns.post_v0.4']['service'].stop_workers()
         del service_store['foo_ns.post_v0.4']
-#
-#
-# def test_get_code_module():
-#     a = adama.service.Service(
-#         name='foo', namespace='', version='', url='http://example.com',
-#         whitelist=[], description='', requirements=[], notify='',
-#         json_path='', adapter='foo.py', code='foo', type='QueryWorker')
-#     a.extract_code()
-#     assert open(
-#         os.path.join(a.temp_dir, 'user_code/foo.py')).read() == 'foo'
-#
-# def test_get_code_tarball():
-#     a = adama.service.Service(
-#         name='foo', namespace='', version='', url='http://example.com',
-#         whitelist=[], description='', requirements=[], notify='',
-#         adapter='foo.tgz', code=open(os.path.join(HERE, 'foo.tgz')).read(),
-#         json_path='', type='QueryWorker')
-#     a.extract_code()
-#     assert open(
-#         os.path.join(a.temp_dir, 'user_code/main.py')).read() == 'foo\n'
-#     assert open(
-#         os.path.join(a.temp_dir, 'user_code/dir/bar')).read() == 'bar\n'
-#
 # def test_get_code_zip():
 #     a = adama.service.Service(
 #         name='foo', namespace='', version='', url='http://example.com',
