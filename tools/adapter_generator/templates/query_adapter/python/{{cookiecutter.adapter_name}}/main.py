b7375b797f373d1b03fe522cf692a7796667e2a8
Walter Moreira <walter@waltermoreira.net>
2014-10-02 17:04:30 -0500

    Start with the adapter generator

---
+++ tools/adapter_generator/templates/query_adapter/python/{{cookiecutter.adapter_name}}/main.py
@@ -0,0 +1,44 @@
+# Adapter: {{ cookiecutter.adapter_name }}
+# Author: {{ cookiecutter.full_name }} <{{ cookiecutter.email }}>
+#
+
+import json
+
+
+def search(args):
+    """Search a data source.
+
+    ``args`` is a dictionary with the query and pagination
+    requested.
+
+    For example, the request::
+
+        /my_namespace/{{ cookiecutter.adapter_name }}/search?key=5
+
+    will result in::
+
+        args = {'key': 5,
+                'count': False,
+                'page': 1,
+                'page_size': None}
+
+    """
+
+    print json.dumps({'this_adapter': "{{ cookiecutter.adapter_name }}"})
+    print '---'
+    print json.dumps({'args': args})
+
+
+def list(args):
+    """List a data source.
+
+    This method implements a listing, if possible, of the data
+    source.
+
+    ``args`` contains only the information for the pagination.
+
+    """
+
+    for i in range(5):
+        print json.dumps({'item': i})
+        print '---'
