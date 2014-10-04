565ea2d9d839573cb8a0d5a4c547a9bf6653dbd8
Walter Moreira <walter@waltermoreira.net>
2014-10-04 17:45:29 -0500

    First working version of adapter generator

--- adama/generator/generator.py
+++ adama/generator/generator.py
@@ -1,13 +1,16 @@
-import sys
+import os
 from textwrap import dedent
 
 from blessings import Terminal
 import click
+from cookiecutter.main import cookiecutter
 
 from adama import __version__
+from adama.tools import location_of
 
 
 t = Terminal()
+HERE = location_of(__file__)
 
 
 @click.group()
@@ -25,19 +28,25 @@ def generator():
 @click.option('--email',
               prompt="{t.green}Your email?{t.normal}".format(t=t))
 @click.option('--type',
+              type=click.Choice(['query', 'map_filter', 'passthrough']),
               prompt=("{t.green}Type of adapter?{t.normal} "
                       "[{t.cyan}query{t.normal}, "
-                      "{t.cyan}map_filter{t.normal}]")
+                      "{t.cyan}map_filter{t.normal}, "
+                      "{t.cyan}passthrough{t.normal}]")
               .format(t=t))
 @click.option('--language',
+              type=click.Choice(['python', 'javascript']),
               prompt=("{t.green}Language of choice?{t.normal} "
                       "[{t.cyan}python{t.normal}, "
                       "{t.cyan}javascript{t.normal}]")
               .format(t=t))
 @click.option('--adapter_name',
               prompt="{t.green}Name for the adapter?{t.normal}".format(t=t))
-def create(name, email, type, language, adapter_name):
-    click.echo(locals())
+def create(**kwargs):
+    directory = os.path.join(
+        HERE, 'templates', kwargs['type'], kwargs['language'])
+    cookiecutter(directory, no_input=True, extra_context=kwargs)
+    click.echo('{t.bold_yellow}Done!{t.normal}'.format(t=t))
 
 
 @generator.command()
