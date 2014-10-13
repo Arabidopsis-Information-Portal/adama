c13d3dca94fd4ac0a7992f2e21690b39bdbf9639
Walter Moreira <walter@waltermoreira.net>
2014-10-13 12:04:15 -0500

    Add docs for status endpoint

--- adama/status.py
+++ adama/status.py
@@ -1,15 +1,45 @@
 import subprocess
 
 from flask.ext import restful
+from adama.swagger import swagger
 
 from . import __version__
 from .api import ok
 from .tools import location_of
 
 
+@swagger.model
+class StatusModel(object):
+    """Status response."""
+
+    resource_fields = {
+        'status': restful.fields.String(attribute='success or error'),
+        'api': restful.fields.String(attribute='version of the API'),
+        'hash': restful.fields.String(
+            attribute='commit hash of Adama server currently running')
+    }
+
 class StatusResource(restful.Resource):
 
+    @swagger.operation(
+        notes="Return the status of the Adama server.",
+        responseClass=StatusModel.__name__,
+        nickname='getStatus',
+        parameters=[],
+        responseMessages=[
+            {
+                'code': 200,
+                'message': 'server is running'
+            },
+            {
+                'code': 504,
+                'message': 'server is down'
+            }
+        ]
+    )
     def get(self):
+        """Return status of the server"""
+
         return ok({
             'api': 'Adama v{}'.format(__version__),
             'hash': head_hash()
