fe24be885aef40a5579ea51dc1da41f48143ebf2
Walter Moreira <walter@waltermoreira.net>
2014-10-20 11:22:58 -0500

    First pass at checking jwt

--- adama/routes.py
+++ adama/routes.py
@@ -1,6 +1,9 @@
+import base64
 import re
 
 from flask import render_template, redirect, url_for, request, abort
+from Crypto.PublicKey import RSA
+import jwt
 
 from . import app
 from .api import api
@@ -49,15 +52,44 @@ def root():
     return redirect(url_for('hello_world'))
 
 
+@app.before_request
+def check_access():
+    # don't control access to OPTIONS verb
+    if request.method == 'OPTIONS':
+        return
+    access_control_type = Config.get('server', 'access_control')
+    if access_control_type == 'none':
+        return
+    if access_control_type == 'jwt':
+        return check_jwt(request)
+    if access_control_type == 'bearer_token':
+        return check_bearer_token(request)
+    abort(400)
+
+
+def get_pub_key():
+    pub_key = Config.get('server', 'apim_public_key')
+    return RSA.importKey(base64.b64decode(pub_key))
+
+
+PUB_KEY = get_pub_key()
+
+def check_jwt(request):
+    tenant_name = Config.get('server', 'tenant_name')
+    try:
+        decoded = jwt.decode(
+            request.headers['X-JWT-Assertion-{0}'.format(tenant_name)],
+            PUB_KEY)
+    except (jwt.DecodeError, KeyError):
+        abort(400)
+
+
 TOKEN_RE = re.compile('Bearer (.+)')
 
-@app.before_request
-def check_token():
-    if not Config.getboolean('server', 'auth'):
-        return
+def check_bearer_token(request):
     # --- REVIEW THIS ---
     # Allow unauthorized GET requests for now
-    if request.method in ('GET', 'OPTIONS'):
+    if request.method == 'GET':
         return
     # ------
     # bypass auth in /json and non-prefixed urls
