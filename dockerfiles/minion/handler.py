57aa62079483c6e1a727a546fcacb18ac72f4594
Walter Moreira <walter@waltermoreira.net>
2014-11-19 14:04:58 -0600

    Minion is now responding to start commands

--- dockerfiles/minion/handler.py
+++ dockerfiles/minion/handler.py
@@ -1,14 +1,61 @@
 #!/usr/bin/env python
+from functools import wraps
+import os
 import sys
+import subprocess
+import cStringIO
 
 from serf_master import SerfHandler, SerfHandlerProxy
 
 
+MAX_OUTPUT = 1000
+
+
+def truncated_stdout(f):
+    """Decorator to truncate stdout to final `MAX_OUTPUT` characters. """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        old_stdout = sys.stdout
+        old_stdout.flush()
+        sys.stdout = cStringIO.StringIO()
+        try:
+            return f(*args, **kwargs)
+        finally:
+            out = sys.stdout.getvalue()
+            sys.stdout = old_stdout
+            print out[-MAX_OUTPUT:]
+    return wrapper
+
+
 class MinionHandler(SerfHandler):
 
+    @truncated_stdout
     def start(self):
-        payload = sys.stdin.read()
-        print 'got payload = {}'.format(payload)
+        image, num_workers = sys.stdin.read().split()
+        registry = os.environ['LOCAL_REGISTRY']
+        remote_image = '{}/{}'.format(registry, image)
+        docker('pull', remote_image)
+        for i in range(int(num_workers)):
+            c = docker('run', '-d', remote_image, 'sleep', 'infinity')
+            print c
+
+
+def path(p):
+    """Build the corresponding path `p` inside the container. """
+
+    return os.path.normpath(os.path.join('/host', './{}'.format(p)))
+
+
+def docker(*args):
+    """Execute a docker command inside the container. """
+
+    docker_binary = path(os.environ.get('DOCKER_BINARY', '/usr/bin/docker'))
+    docker_socket = path(os.environ.get('DOCKER_SOCKET', '/run/docker.sock'))
+    cmd = ('{docker_binary} -H unix://{docker_socket}'
+           .format(**locals()).split())
+    cmd.extend(args)
+    return subprocess.check_output(cmd)
 
 
 if __name__ == '__main__':
