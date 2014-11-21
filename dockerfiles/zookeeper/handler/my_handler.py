1036d18b7b65144c85878f8feed6774fa49d73ac
Walter Moreira <walter@waltermoreira.net>
2014-11-21 13:54:33 -0600

    Use volumes to keep zookeeper data and config

--- dockerfiles/zookeeper/handler/my_handler.py
+++ dockerfiles/zookeeper/handler/my_handler.py
@@ -14,7 +14,7 @@ class MyHandler(SerfHandler):
         tickTime=2000
         initLimit=10
         syncLimit=5
-        dataDir={pwd}/zookeeper
+        dataDir=/data/zookeeper
         clientPort=2181
         {servers}
         """)
@@ -25,19 +25,14 @@ class MyHandler(SerfHandler):
         myself = serf('info')['agent']['name']
         sorted_members = sort_members(members['members'])
         my = myid(myself, sorted_members)
-        print >>sys.stderr, 'PWD = {}'.format(os.environ['PWDX'])
-        pwd = os.path.normpath(
-            os.path.join('/host', './{}'.format(os.environ['PWDX'])))
-        print >>sys.stderr, 'pwd = {}'.format(pwd)
-        with open(os.path.join(pwd, 'zoo.cfg'), 'w') as f:
+        with open('/data/zoo.cfg', 'w') as f:
             servers = [
                 'server.{i}={ip}:{port1}:{port2}'
                 .format(i=i+1, ip=elt['addr'].split(':')[0],
                         port1=2888+i, port2=3888+i)
                 for i, elt in enumerate(sorted_members)]
-            f.write(self.TEMPLATE.format(
-                pwd=pwd, servers='\n'.join(servers)))
-        with open(os.path.join(pwd, 'zookeeper/myid'), 'w') as f:
+            f.write(self.TEMPLATE.format(servers='\n'.join(servers)))
+        with open('/data/zookeeper/myid', 'w') as f:
             f.write(str(my))
 
 
@@ -49,8 +44,6 @@ def sort_members(members):
 
 
 def myid(myself, members):
-    print members
-    print myself
     for i, elt in enumerate(members):
         if elt['name'] == myself:
             return i+1
