bff000936e179cb1447a2e93932ac2f64ad9f62d
Walter Moreira <walter@waltermoreira.net>
2014-11-23 23:06:23 -0600

    Allow multiple serf nodes in same machine

--- dockerfiles/serf_node/handler/join.py
+++ dockerfiles/serf_node/handler/join.py
@@ -24,6 +24,15 @@ def main():
     advertise = os.environ.get('ADVERTISE')
     if advertise:
         cmd.extend(['-advertise', advertise])
+    bind_port = os.environ.get('BIND_PORT', '7946')
+    if bind_port:
+        cmd.extend(['-bind', '0.0.0.0:{}'.format(bind_port)])
+    node = os.environ.get('NODE')
+    if node:
+        cmd.extend(['-node', node])
+    rpc_port = os.environ.get('RPC_PORT', '7373')
+    if rpc_port:
+        cmd.extend(['-rpc-addr', '127.0.0.1:{}'.format(rpc_port)])
     subprocess.check_call(cmd)
 
 
