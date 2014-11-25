c2200c9f3a7d3eeea929f3fa79747c9697425594
Walter Moreira <walter@waltermoreira.net>
2014-11-25 13:42:48 -0600

    Add a generic eventlistener
    This listener converts supervisor events to serf events
    so they can be propagated in the cluster.

---
+++ dockerfiles/serfnode/handler/supervisor_to_serf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+from utils import serf_event
+
+
+def write_stdout(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+def write_stderr(s):
+    sys.stderr.write(s)
+    sys.stderr.flush()
+
+def main():
+    while True:
+        write_stdout('READY\n') # transition from ACKNOWLEDGED to READY
+        line = sys.stdin.readline()  # read header line from stdin
+        headers = dict(x.split(':') for x in line.split())
+        data = sys.stdin.read(int(headers['len'])) # read the event payload
+        data_dict = dict(x.split(':') for x in data.split())
+        data_dict['eventname'] = headers['eventname']
+        serf_event('myevent', json.dumps(data_dict))
+        write_stdout('RESULT 2\nOK') # transition from READY to ACKNOWLEDGED
+
+if __name__ == '__main__':
+    main()
