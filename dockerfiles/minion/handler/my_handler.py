e989a8adb37e6b56a384f1c08c4a55cdd0fe5eae
Walter Moreira <walter@waltermoreira.net>
2014-11-29 23:54:55 -0600

    Ignore start events not for this minion node

--- dockerfiles/minion/handler/my_handler.py
+++ dockerfiles/minion/handler/my_handler.py
@@ -5,14 +5,18 @@ import os
 from serf_master import SerfHandler
 import docker_utils
 import firewall
-from utils import truncated_stdout, with_payload
+import supervisor
+from utils import truncated_stdout, with_payload, is_self
 
 
 class MyHandler(SerfHandler):
 
     @truncated_stdout
     @with_payload
-    def start(self, image=None, num_workers=None, args=None):
+    def start(self, image=None, num_workers=None, args=None, **kwargs):
+        node = kwargs.get('node', None)
+        if node is not None and not is_self(node):
+            return
         args = args or []
         remote_image = remote(image)
         docker_utils.docker('pull', remote_image)
