1065a9c94aa48141de3c732cb06f21880a77885c
Walter Moreira <walter@waltermoreira.net>
2014-12-02 12:56:54 -0600

    Showcase new features in serfnode example

--- dockerfiles/example/handler/my_handler.py
+++ dockerfiles/example/handler/my_handler.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
-import json
 import pprint
-import sys
 
 from base_handler import BaseHandler
+from utils import with_payload, with_member_info
 
 
 class MyHandler(BaseHandler):
 
-    def supervisor(self):
+    def setup(self):
+        """This gets run when the handler is created."""
+        pass
+
+    @with_payload
+    def supervisor(self, **kwargs):
+        """Event or query ``supervisor`` with JSON payload."""
+
         print 'Got a supervisor event with payload:'
-        pprint.pprint(json.loads(sys.stdin.read()))
+        pprint.pprint(kwargs)
+
+    @with_member_info
+    def member_join(self, members):
+        """``members`` is a dict with the nodes that joined."""
+
+        print 'New members joined:'
+        pprint.pprint(members)
