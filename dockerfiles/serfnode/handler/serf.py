fc48d95feae51cadb716bbbec3aa873bd2907084
Walter Moreira <walter@waltermoreira.net>
2014-12-15 17:26:47 -0600

    Add function to retrieve actor address of node

--- dockerfiles/serfnode/handler/serf.py
+++ dockerfiles/serfnode/handler/serf.py
@@ -21,16 +21,24 @@ def serf_event(name, *args):
     subprocess.check_call(cmd, stdout=sys.stderr)
 
 
-def where(service):
+def _query(name, service):
     rpc_port = info.NODE_INFO['rpc_port']
     cmd = ['serf', 'query',
            '-rpc-addr', '127.0.0.1:{}'.format(rpc_port),
-           '-format=json', 'where',
+           '-format=json', name,
            json.dumps({'role': service})]
     out = json.loads(subprocess.check_output(cmd))
     for node, response in out['Responses'].items():
         if response.endswith('SUCCESS'):
             yield json.loads(response[:-len('SUCCESS')])
+
+
+def where(service):
+    return _query('where', service)
+
+
+def where_actor(service):
+    return _query('where_actor', service)
 
 
 def is_self(node):
