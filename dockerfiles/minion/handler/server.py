b38d0c6a78226d0d30e46096a1f010da2d5a8775
Walter Moreira <walter@waltermoreira.net>
2014-12-15 14:11:07 -0600

    Server writes actor info to filesystem

--- dockerfiles/minion/handler/server.py
+++ dockerfiles/minion/handler/server.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
+import json
+import os
+import sys
+
+import mischief.actors.pipe as p
 import mischief.actors.actor as a
 import server_actor
 
 
 if __name__ == '__main__':
+    ip = os.environ['IP'] or p.get_local_ip('8.8.8.8')
+    print('IP = {}'.format(ip), file=sys.stderr)
     with a.Wait() as w:
-        a.spawn(server_actor.MinionServer)
+        proc = a.spawn(server_actor.MinionServer, ip=ip)
+        with open('/actor_info', 'w') as f:
+            name, ip, port = proc.address()
+            f.write(json.dumps({
+                'name': name,
+                'ip': ip,
+                'port': port}))
         w.act()
