17916c3d8a2692139a99a40b09f11e570976edc5
Walter Moreira <walter@waltermoreira.net>
2015-01-13 16:59:47 -0600

    Update roles with serfnode_init update

--- components/redis/deploy/deploy.sh
+++ components/redis/deploy/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-EXTRA_VARS="$*"
-ansible-playbook -i hosts --extra-vars "\"$EXTRA_VARS\"" deploy.yml
+cd $(dirname $0)
+TEMPFILE=$(mktemp || mktemp -t X) 2>/dev/null
+EXTRA_VARS="tempfile=$TEMPFILE $*"
+eval ansible-playbook -i hosts --extra-vars "\"$EXTRA_VARS\"" deploy.yml >&2
+cat $TEMPFILE
+rm -rf $TEMPFILE
