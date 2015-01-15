ad98584944ab88683bb8fadd101411ecdc21146f
Walter Moreira <walter@waltermoreira.net>
2015-01-15 15:13:35 -0600

    Apply new deploy from serfnode

--- components/rabbitmq/deploy/deploy.sh
+++ components/rabbitmq/deploy/deploy.sh
@@ -2,7 +2,10 @@
 
 cd $(dirname $0)
 TEMPFILE=$(mktemp || mktemp -t X) 2>/dev/null
-EXTRA_VARS="tempfile=$TEMPFILE $*"
-eval ansible-playbook -i hosts --extra-vars "\"$EXTRA_VARS\"" deploy.yml >&2
+for var in $*; do
+    EXTRA_VARS="$EXTRA_VARS --extra-vars \"$var\"";
+done
+EXTRA_VARS="$EXTRA_VARS --extra-vars \"tempfile=$TEMPFILE\""
+eval ansible-playbook -i hosts $EXTRA_VARS deploy.yml >&2
 cat $TEMPFILE
 rm -rf $TEMPFILE
