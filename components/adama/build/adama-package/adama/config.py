07501347e1750336e43a6822c407c021a895687c
Walter Moreira <walter@waltermoreira.net>
2015-01-21 11:00:07 -0600

    Config reads yaml file rather than .ini

--- components/adama/build/adama-package/adama/config.py
+++ components/adama/build/adama-package/adama/config.py
@@ -1,26 +1,26 @@
-"""Read config files from:
+"""Read config files from /adama-package/conf.yml """
 
-- Deployed adama.conf inside package
-- /etc/adama.conf
+from typing import Dict, cast
 
-"""
+import yaml
 
-import ConfigParser
-import os
-import sys
-
-from .tools import location_of
-
-HERE = location_of(__file__)
+from .exceptions import AdamaError
 
 
-def read_config():
-    parser = ConfigParser.ConfigParser()
-    places = [os.path.abspath(os.path.join(HERE, '../adama.conf')),
-              os.path.expanduser('/etc/adama.conf')]
-    if not parser.read(places):
-        raise RuntimeError("couldn't read config file from {0}"
-                           .format(', '.join(places)))
-    return parser
+def read() -> Dict:
+    conf_file = open('/adama-package/conf.yml')
+    try:
+        conf_yaml = yaml.load(conf_file)
+        assert isinstance(conf_yaml, dict)
+        return cast(Dict, conf_yaml)
+    except yaml.error.YAMLError as exc:
+        raise AdamaError(
+            'wrong yaml in /adama-package/conf.yml\n'
+            '{}'.format(exc))
+    except AssertionError:
+        raise AdamaError(
+            'config file /adama-package/conf.yml should be an object, '
+            'not string, number, or list')
 
-Config = read_config()
+
+Config = read()
