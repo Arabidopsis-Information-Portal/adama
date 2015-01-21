0b7ad7eab48504a338a2a8af7498a6cf8dec5096
Walter Moreira <walter@waltermoreira.net>
2015-01-21 10:59:39 -0600

    Simplify code for working in container

--- components/adama/build/adama-package/adama/__init__.py
+++ components/adama/build/adama-package/adama/__init__.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
-import os
-
-from .tools import location_of
-
-HERE = location_of(__file__)
 
 __author__ = 'Walter Moreira'
 __email__ = 'wmoreira@tacc.utexas.edu'
-__version__ = open(os.path.join(HERE, 'VERSION')).read().strip()
+__version__ = open('/adama-package/adama/VERSION').read().strip()
 
 from flask import Flask
 
