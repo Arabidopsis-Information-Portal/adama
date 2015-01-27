251c31b23e45f5d875ad4c6bb81e90c62ed06793
Walter Moreira <walter@waltermoreira.net>
2015-01-27 11:30:18 -0600

    Remove dummy handler

--- components/adama/build/handler/my_handler.py
+++ components/adama/build/handler/my_handler.py
@@ -6,6 +6,3 @@ class MyHandler(BaseHandler):
 
     def setup(self):
         super(MyHandler, self).setup()
-        supervisor.start_docker(
-            'app.conf',
-            '--rm {} ubuntu sleep infinity'.format(self.volumes))
