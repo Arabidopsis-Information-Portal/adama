ef4ca18064db8f45d687b4d521e97907fd5bd226
Walter Moreira <walter@waltermoreira.net>
2015-02-03 10:59:19 -0600

    Improve dodo file for idempotence

--- components/redis-try/dodo.py
+++ components/redis-try/dodo.py
@@ -1,22 +1,29 @@
 import os
+import subprocess
 
 from doit.tools import result_dep, title_with_actions
 
 
 def task__check_images():
-    for img in ('serfnode', 'serf-redis'):
-        yield {
-            'actions': ["docker inspect -f '{{ .Id }}' " + img +
-                        " 2>/dev/null || echo''"],
-            'name': img
-        }
+    img = 'serfnode'
+    return {
+        'actions': ["docker inspect -f '{{{{ .Id }}}}' {} "
+                    "2>/dev/null || echo ''".format(img)],
+    }
+
+
+def target_image_exists():
+    try:
+        subprocess.check_output(
+            'docker inspect serf-redis 1>/dev/null 2>&1',
+            shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 def task_build():
     """Build serf-redis image"""
-
-    def hi():
-        print('Hi there')
 
     all_files = ['dodo.py', 'Dockerfile']
     for d, _, fs in os.walk('handler'):
@@ -24,8 +31,9 @@ def task_build():
             all_files.append(os.path.join(d, f))
 
     return {
-        'actions': ['touch .build'],
+        'actions': ['docker build -t serf-redis .',
+                    'touch .build'],
         'targets': ['.build'],
         'file_dep': all_files,
-        'uptodate': [result_dep('_check_images')]
+        'uptodate': [result_dep('_check_images'), target_image_exists]
     }
