c8ca091a190ae7da7b5536395b4694bcb0e02fd7
Walter Moreira <walter@waltermoreira.net>
2015-02-03 10:13:58 -0600

    Simplify dodo file

--- components/redis-try/dodo.py
+++ components/redis-try/dodo.py
@@ -3,32 +3,13 @@ import os
 from doit.tools import result_dep, title_with_actions
 
 
-def task_hello():
-    """hello"""
-
-    def python_hello(targets):
-        with open(targets[0], "a") as output:
-            output.write("Python says Hello World!!!\n")
-
-    return {
-        'actions': [python_hello],
-        'targets': ["hello.txt"],
+def task__check_images():
+    for img in ('serfnode', 'serf-redis'):
+        yield {
+            'actions': ["docker inspect -f '{{ .Id }}' " + img +
+                        " 2>/dev/null || echo''"],
+            'name': img
         }
-
-
-def task__serfnode_version():
-    return {
-        'actions': ["docker inspect -f '{{ .Id }}' serfnode"]
-    }
-
-
-def task__redis_image_exists():
-    """Check that serf-redis image exists"""
-    return {
-        'actions': ["docker inspect -f '{{ .Id }}' serf-redis "
-                    "2>/dev/null || echo ''"],
-        'title': lambda x: 'foo' + str(x)
-    }
 
 
 def task_build():
@@ -43,8 +24,8 @@ def task_build():
             all_files.append(os.path.join(d, f))
 
     return {
-        'actions': ['touch foo'],
+        'actions': ['touch .build'],
+        'targets': ['.build'],
         'file_dep': all_files,
-        'uptodate': [result_dep('_serfnode_version'),
-                     result_dep('_redis_image_exists')]
+        'uptodate': [result_dep('_check_images')]
     }
