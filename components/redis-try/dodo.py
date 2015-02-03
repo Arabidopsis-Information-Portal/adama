13782e7b530f0e0b3aec75db7ae255e525ee808c
Walter Moreira <walter@waltermoreira.net>
2015-02-03 14:16:12 -0600

    More improvements in build script

--- components/redis-try/dodo.py
+++ components/redis-try/dodo.py
@@ -4,28 +4,51 @@ import subprocess
 from doit.tools import result_dep, title_with_actions
 
 
+DOIT_CONFIG = {'default_tasks': ['build']}
+
+
 def task__check_images():
+    """Check dependencies in the form of images"""
+
     img = 'serfnode'
     return {
         'actions': ["docker inspect -f '{{{{ .Id }}}}' {} "
-                    "2>/dev/null || echo ''".format(img)],
+                    "2>/dev/null".format(img)]
     }
 
 
-def target_image_exists():
-    try:
-        subprocess.check_output(
-            'docker inspect serf-redis 1>/dev/null 2>&1',
-            shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+def target_image_exists(img):
+    """Check if 'img' exists in local registry"""
+
+    def f():
+        try:
+            subprocess.check_output(
+                'docker inspect {} 1>/dev/null 2>&1'.format(img),
+                shell=True)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+    return f
+
+
+def remote_image_exists(img):
+    """Check if 'img' exists in remote registry"""
+
+    def f():
+        try:
+            subprocess.check_output(
+                'docker pull {} 1>/dev/null 2>&1'.format(img),
+                shell=True)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+    return f
 
 
 def task_build():
     """Build serf-redis image"""
 
-    all_files = ['dodo.py', 'Dockerfile']
+    all_files = ['dodo.py', 'Dockerfile', 'deploy.yml']
     for d, _, fs in os.walk('handler'):
         for f in fs:
             all_files.append(os.path.join(d, f))
@@ -35,5 +58,23 @@ def task_build():
                     'touch .build'],
         'targets': ['.build'],
         'file_dep': all_files,
-        'uptodate': [result_dep('_check_images'), target_image_exists]
+        'task_dep': ['_check_images'],
+        'uptodate': [result_dep('_check_images'),
+                     target_image_exists('serf-redis')],
+        'verbosity': 2
+    }
+
+
+def task_push():
+    """Push image to docker hub"""
+
+    return {
+        'actions': ['docker tag -f serf-redis waltermoreira/serf-redis',
+                    'docker push waltermoreira/serf-redis',
+                    'touch .push'],
+        'targets': ['.push'],
+        'file_dep': ['.build'],
+        'task_dep': ['build'],
+        'uptodate': [remote_image_exists('waltermoreira/serf-redis')],
+        'verbosity': 2
     }
