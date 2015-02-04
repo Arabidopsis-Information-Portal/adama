3c61edd5217633563fe22ce8f1d96be870555c2e
Walter Moreira <walter@waltermoreira.net>
2015-02-04 10:45:44 -0600

    Fix wrong name for rabbitmq supervisor stanza

--- components/rabbitmq/handler/serfnode.py
+++ components/rabbitmq/handler/serfnode.py
@@ -3,4 +3,4 @@ import supervisor
 
 def spawn(volumes):
     supervisor.install_launcher(
-        'redis', '{} dockerfile/rabbitmq'.format(volumes))
+        'rabbitmq', '{} dockerfile/rabbitmq'.format(volumes))
