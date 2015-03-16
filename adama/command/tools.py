49069c9d9f49fa4e7c497d2f1de326162f443666
Walter Moreira <walter@waltermoreira.net>
2015-03-16 12:46:53 -0500

    Back up code from container
    Use code from the container rather than checked-out code from git,
    since it may have been removed from local filesystem.

--- adama/command/tools.py
+++ adama/command/tools.py
@@ -3,9 +3,11 @@ import os
 import subprocess
 import tarfile
 import time
+import tempfile
 
 from ..service_store import service_store
 from ..firewall import Firewall
+from ..docker import safe_docker
 
 
 def rebuild_service(name):
@@ -73,8 +75,14 @@ def _backup_code(srv, destination):
     adapters_path = os.path.join(destination, 'adapters')
     subprocess.check_call('mkdir -p {}'.format(adapters_path).split())
     target = os.path.join(adapters_path, srv.iden+'.tar.bz2')
+    try:
+        worker = srv.workers[0]
+        tempdir = tempfile.mkdtemp()
+        safe_docker('cp', '{}:/root/user_code'.format(worker), tempdir)
+    except IndexError:
+        return
     with tarfile.open(target, 'w:bz2') as tar:
-        tar.add(srv.code_dir)
+        tar.add(tempdir)
 
 
 def backup_code(destination):
