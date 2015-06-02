3ec949e2d874365b18daa1182e74ab0de9380994
Walter Moreira <walter@waltermoreira.net>
2015-06-02 15:39:49 -0500

    Make minion server an executable

--- adama/minion.py
+++ adama/minion.py
@@ -1,17 +1,39 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
 import json
 import time
+import os
+import sys
 
-from tasks import QueueConnection
+from tasks import QueueConnection, SimpleProducer
 
 
 class Minion(QueueConnection):
 
     def callback(self, message, responder):
-        print 'Got', message
+        print('Got {}'.format(message))
         time.sleep(10)
         responder(json.dumps({'ok': True}))
 
     def run(self):
+        print("Starting minion server with queue {}:{} "
+              "and channel 'minion_server'"
+              .format(self.queue_host, self.queue_port),
+              file=sys.stderr)
         self.consume_forever(self.callback)
 
 
+def main():
+    host, port = os.environ['RABBITMQ_SERVER'].split(':')
+    minion = Minion(host, int(port), 'minion_server')
+    minion.run()
+
+
+def client():
+    host, port = os.environ['RABBITMQ_SERVER'].split(':')
+    return SimpleProducer(host, int(port), 'minion_server')
+
+
+if __name__ == '__main__':
+    main()
