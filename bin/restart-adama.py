0a92ca6ac5b6e037fd6796cb4688468b9602e7ce
Walter Moreira <walter@waltermoreira.net>
2015-06-08 15:09:18 -0500

    Add scripts to restart adama and workers

---
+++ bin/restart-adama.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess
+import time
+
+
+def main():
+    subprocess.check_call('supervisorctl stop adama'.split())
+    time.sleep(2)  # give time to clean
+    subprocess.check_call('supervisorctl start adama'.split())
+    print 'ok'
+
+
+if __name__ == '__main__':
+   main()
---
+++ bin/restart-workers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+
+import adama.command.tools as t
+import adama.service_store as s
+
+
+def main():
+    for srv in s.service_store:
+        print 'Restarting', srv, '...',
+        t.restart_workers(srv)
+        print 'ok'
+        time.sleep(0.1)
+
+
+if __name__ == '__main__':
+   main()
