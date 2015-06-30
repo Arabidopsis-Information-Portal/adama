da58b8d90f9e42e427913841be75a3b45c709b33
Walter Moreira <walter@waltermoreira.net>
2015-06-30 14:17:23 -0500

    Start module for monitoring workers

---
+++ adama/command/worker_monitor.py
@@ -0,0 +1,37 @@
+import docker
+
+from .tools import workers_of
+
+
+def is_ready(cid):
+    """True if the container is 'ready'.
+
+    Ready means able to consume messages from the queue.
+
+    :type cid: str
+    :rtype: bool
+    """
+    client = docker.Client()
+    try:
+        info = client.inspect_container(cid)
+    except docker.errors.APIError:
+        return False
+    if not info['State']['Running']:
+        return False
+    cat = client.exec_create(
+        cid, ['cat', '/busy'], stdout=True, stderr=True)
+    client.exec_start(cat['Id'])
+    exit_code = client.exec_inspect(cat['Id'])['ExitCode']
+    # if there is no file /busy, the worker is ready
+    return exit_code != 0
+
+
+def workers_ready_for(service_name):
+    """Return number of workers currently ready for the service.
+
+     `srv` is the full service name (namespace + service + version).
+
+    :type service: str
+    :rtype: int
+    """
+    return len(filter(is_ready, workers_of(service_name)))
