1aabf6e17d9b055318121d389962a810a7236d9f
Walter Moreira <walter@waltermoreira.net>
2015-07-09 16:08:30 -0500

    Move docker client as a global to act as singleton
    This avoid connecting too many times to the daemon, if the checks are
    frequent.

--- adama/command/worker_monitor.py
+++ adama/command/worker_monitor.py
@@ -5,15 +5,17 @@ from .tools import workers_of
 from ..tasks import QueueConnection
 
 
+DOCKER_CLIENT = docker.Client(version='auto')
+
+
 def is_up(cid):
     """True if container is running.
 
     :type cid: str
     :rtype: bool
     """
-    client = docker.Client(version='auto')
     try:
-        info = client.inspect_container(cid)
+        info = DOCKER_CLIENT.inspect_container(cid)
     except docker.errors.APIError:
         return False
     return info['State']['Running']
@@ -29,11 +31,10 @@ def is_ready(cid):
     """
     if not is_up(cid):
         return False
-    client = docker.Client(version='auto')
-    cat = client.exec_create(
+    cat = DOCKER_CLIENT.exec_create(
         cid, ['cat', '/busy'], stdout=True, stderr=True)
-    client.exec_start(cat['Id'])
-    exit_code = client.exec_inspect(cat['Id'])['ExitCode']
+    DOCKER_CLIENT.exec_start(cat['Id'])
+    exit_code = DOCKER_CLIENT.exec_inspect(cat['Id'])['ExitCode']
     # if there is no file /busy, the worker is ready
     return exit_code != 0
 
