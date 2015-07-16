ef3ee0b1527776ac5a418f1b2143dc2aff022663
Walter Moreira <walter@waltermoreira.net>
2015-07-16 11:19:09 -0500

    More progress in image_builder service

--- components/image_builder/app/main.py
+++ components/image_builder/app/main.py
@@ -1,18 +1,43 @@
+#!/usr/bin/env python
+
 import os
 
 from channelpy import Channel
 
 
-URI = os.environ.get('BROKER', 'amqp://172.17.42.1:5672')
+URI = 'amqp://rabbit:5672'
+
+
+def error(msg, ch):
+    ch.put(
+        {
+        }
+    )
+
 
 def main():
-    listen = Channel(uri=URI)
-    while True:
-        job = listen.get()
-        args = job['args']
-        namespace = job['namespace']
-        reply_to = job['reply_to']
-        reply_to.put('will process {} and {}'.format(args, namespace))
+    with Channel(name='image_builder', uri=URI, persist=True) as listen:
+        while True:
+            job = listen.get()
+
+            try:
+                reply_to = job['reply_to']
+                assert isinstance(reply_to, Channel)
+            except (KeyError, AssertionError):
+                print("invalid or missing 'reply_to': {}".format(job))
+                continue
+
+            try:
+                args = job['args']
+                namespace = job['namespace']
+            except KeyError:
+                error("missing 'args' and/or 'namespace': {}".format(job),
+                      reply_to)
+                continue
+
+            reply_to.put(
+                '[{}] will process {} and {}'
+                    .format(os.getpid(), args, namespace))
 
 
 if __name__ == '__main__':
