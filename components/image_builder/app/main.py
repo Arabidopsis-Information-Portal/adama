ced3df321eaab93468e67e92bdaa5b5717cd3059
Walter Moreira <walter@waltermoreira.net>
2015-07-23 17:01:09 -0500

    Use proper context management

--- components/image_builder/app/main.py
+++ components/image_builder/app/main.py
@@ -8,38 +8,32 @@ from channelpy import Channel, RabbitConnection
 URI = 'amqp://rabbit:5672'
 
 
-def error(msg, ch):
-    ch.put(
-        {
-        }
-    )
+def error(msg, code, ch):
+    ch.put({
+        'status': 'error',
+        'message': msg,
+        'code': code
+    })
 
 
 def main():
-    with Channel(name='image_builder', persist=True,
+    with Channel(name='image_builder', persist=False,
                  connection_type=RabbitConnection,
                  uri=URI) as listen:
         while True:
             job = listen.get()
+            with job['reply_to'] as reply_to:
+                try:
+                    args = job['args']
+                    namespace = job['namespace']
+                except KeyError:
+                    error("missing 'args' and/or 'namespace': {}".format(job),
+                          400, reply_to)
+                    continue
 
-            try:
-                reply_to = job['reply_to']
-                assert isinstance(reply_to, Channel)
-            except (KeyError, AssertionError):
-                print("invalid or missing 'reply_to': {}".format(job))
-                continue
-
-            try:
-                args = job['args']
-                namespace = job['namespace']
-            except KeyError:
-                error("missing 'args' and/or 'namespace': {}".format(job),
-                      reply_to)
-                continue
-
-            reply_to.put(
-                '[{}] will process {} and {}'
-                    .format(os.getpid(), args, namespace))
+                reply_to.put(
+                    '[{}] will process {} and {}'
+                        .format(os.getpid(), args, namespace))
 
 
 if __name__ == '__main__':
