b7ec8a94ea784099d3f16412cb243c39e15cbfe6
Walter Moreira <walter@waltermoreira.net>
2015-07-27 17:23:37 -0500

    Progress with the image_builder

--- docker/image_builder/app/image_builder.py
+++ docker/image_builder/app/image_builder.py
@@ -2,6 +2,8 @@
 
 import os
 import uuid
+import threading
+from traceback import format_exc
 
 from channelpy import Channel, RabbitConnection
 
@@ -32,7 +34,7 @@ def error(msg, code, ch):
     })
 
 
-def handle(guid, args, namespace):
+def start_registration(guid, args, namespace):
     """
     :type guid: str
     :type args: Dict
@@ -41,11 +43,35 @@ def handle(guid, args, namespace):
     REGISTRATION_STORE[guid] = {
         'status': 'starting'
     }
-    spawn()
+    try:
+        service = do_register(args, namespace)
+        REGISTRATION_STORE[guid] = {
+            'status': 'success',
+            'service': service.iden
+        }
+        # notify success
+    except Exception as exc:
+        REGISTRATION_STORE[guid] = {
+            'status': 'failed',
+            'message': str(exc),
+            'traceback': format_exc()
+        }
+        # notify failure
 
 
-def spawn():
-    pass
+def do_register(args, namespace):
+    """
+    :type args: Dict
+    :type namespace: str
+    :rtype: Service
+    """
+    if 'code' in args or args.get('type') == 'passthrough':
+        service = register_code(args, namespace)
+    elif 'git_repository' in args:
+        service = register_git_repository(args, namespace)
+    else:
+        raise APIException('no code or git repository specified')
+    return service
 
 
 def main():
@@ -54,22 +80,29 @@ def main():
                  uri=RABBIT_URI) as listen:
         while True:
             job = listen.get()
-            with job['reply_to'] as reply_to:
-                try:
-                    args = job['value']['args']
-                    namespace = job['value']['namespace']
-                except KeyError:
-                    error("missing 'args' and/or 'namespace': {}".format(job),
-                          400, reply_to)
-                    continue
+            t = threading.Thread(target=process, args=(job,))
+            t.start()
 
-                guid = uuid.uuid4().hex
-                handle(guid, args, namespace)
 
-                reply_to.put({
-                    'message': guid,
-                    'status': 'ok'
-                })
+def process(job):
+    """
+    :type job: Dict
+    """
+    with job['reply_to'] as reply_to:
+        try:
+            args = job['value']['args']
+            namespace = job['value']['namespace']
+        except KeyError:
+            error("missing 'args' and/or 'namespace': {}".format(job),
+                  400, reply_to)
+            return
+
+        guid = uuid.uuid4().hex
+        reply_to.put({
+            'message': guid,
+            'status': 'ok'
+        })
+        start_registration(guid, args, namespace)
 
 
 if __name__ == '__main__':
