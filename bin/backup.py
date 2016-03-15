ab6bb4b5ca4f45b71169d67da124364b9d6e4384
Matthew Vaughn <vaughn@tacc.utexas.edu>
2016-03-15 13:28:52 -0500

    in progress...

--- bin/backup.py
+++ bin/backup.py
@@ -2,8 +2,19 @@
 import os
 import datetime
 import tarfile
+import ConfigParser
 
 import adama.command.tools as t
+
+
+def config():
+    parser = ConfigParser.ConfigParser()
+    here = os.path.dirname(os.path.abspath(__file__))
+    parser.read(os.path.join(here, 'backup.conf'))
+    return parser
+
+
+CONFIG = config()
 
 
 def directory():
@@ -39,8 +50,13 @@ def archive(path):
     :type path: str
     :rtype: None
     """
-    pass
-
+    host_login = CONFIG.get('scp', 'login')
+    host_path = CONFIG.get('scp', 'path')
+    backup_cmd = 'scp {} ' + host_login + ':' + host_path
+    
+    subprocess.check_call(backup_cmd.format(path).split(), stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT) 
+    os.unlink(path)
+    shutil.rmtree(directory)
 
 def main():
     d = directory()
